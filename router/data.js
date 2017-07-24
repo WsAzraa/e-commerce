221580e4fef427772d211b6c7f2ffa32998af055
yebin <yebin@bayax.cn>
2017-07-24 15:24:04 +0800

    测试接口  bannner

--- router/data.js
+++ router/data.js
@@ -3,19 +3,30 @@ var router = express.Router();
 
 // 该路由使用的中间件
 router.use(function timeLog(req, res, next) {
-  console.log('Time: ', Date.now());
-  next();
+	console.log('Time: ', Date.now());
+	next();
 });
-// 定义网站主页的路由
+// // 定义网站主页的路由
 router.get('/data/banner', function(req, res) {
- 	
+	res.json({
+		success: true,
+		data: [{
+			url: 'http://s.amazeui.org/media/i/demos/bing-1.jpg',
+		}, {
+			url: 'http://s.amazeui.org/media/i/demos/bing-2.jpg',
+		}, {
+			url: 'http://s.amazeui.org/media/i/demos/bing-3.jpg',
+		}, {
+			url: 'http://s.amazeui.org/media/i/demos/bing-4.jpg'
+		}]
+	});
 });
-// router.get('/')
+router.get('/')
 
 
 // 定义 about 页面的路由
 router.get('/about', function(req, res) {
-  res.send('About birds');
+	res.send('About birds');
 });
 
 module.exports = router;
--- shop/js/index.js
+++ shop/js/index.js
@@ -1,72 +1,99 @@
-$(function () {
+$(function() {
 
-function getData(page){
-    $.ajax({
-        type:'get',
-        url:'/getdata',
-        data:{page:page},
-        dataType:'json',
-        beforeSend:function(){
-            $('.tips').addClass('loading')
-        },
-        success:function(info){
-            console.log(info);
-            $('.tips').attr('data-page',info.page)
-            for (var i=0; i<info.data.length;i++) {
-            app.items.push(info.data[i])
-            }
-        },
-        complete:function(){
-            $('.tips').removeClass('loading')
+    function getData(page) {
+        // $.ajax({
+        //     type: 'get',
+        //     url: '/getdata',
+        //     data: {
+        //         page: page
+        //     },
+        //     dataType: 'json',
+        //     beforeSend: function() {
+        //         $('.tips').addClass('loading')
+        //     },
+        //     success: function(info) {
+        //         console.log(info);
+        //         $('.tips').attr('data-page', info.page)
+        //         for (var i = 0; i < info.data.length; i++) {
+        //             app.items.push(info.data[i])
+        //         }
+        //     },
+        //     complete: function() {
+        //         $('.tips').removeClass('loading')
+        //     }
+        // })
+        // 
+      
+    }
+
+    // 首次数据加载
+    getData(1);
+
+
+    $(window).scroll(function() {
+
+        // console.log($(window).scrollTop())
+        // console.log($('.banner').height())
+        // console.log($(window).height())
+        // console.log($('.banner').offset().top)
+        var H = ($('.banner').offset().top + $('.banner').height()) - ($(window).height() + $(window).scrollTop())
+        console.log(H)
+        if (H <= 10 && !$('.tips').hasClass('loading')) {
+
+            var page = $('.tips').attr('data-page')
+
+            getData(page)
+
+            console.log('显示了')
+
         }
     })
-}
-
-// 首次数据加载
-getData(1);
-
-
-$(window).scroll(function(){
-
-// console.log($(window).scrollTop())
-// console.log($('.banner').height())
-// console.log($(window).height())
-// console.log($('.banner').offset().top)
-var H=($('.banner').offset().top+ $('.banner').height()) - ($(window).height()+$(window).scrollTop())
-console.log(H)
-      if(H<= 10 && !$('.tips').hasClass('loading')) {
-
-        var page =$('.tips').attr('data-page')
-
-        getData(page)
-        
-        console.log('显示了')
-      
-      }
-})
-
-
-
 
 
 
 });
- // Vue 
-var app= new Vue({
-el:'#app',
-data:{
-    items:[],
-    banners:[
-    {url:"http://s.amazeui.org/media/i/demos/bing-1.jpg"},
-    {url:"http://s.amazeui.org/media/i/demos/bing-2.jpg"},
-    {url:"http://s.amazeui.org/media/i/demos/bing-3.jpg"},
-    {url:"http://s.amazeui.org/media/i/demos/bing-4.jpg"}
-    ],
-    flash:{
-        url:"images/1.jpg_.webp",
-        sprice:"USD  18.2",
-        oldSprice:"USD 44"
+// Vue 
+var app = new Vue({
+    el: '#app',
+    data: {
+        items: [],
+        banners: [{
+            url:'images/a.png'
+        },
+        {
+            url:'images/a.png'
+        },
+        {
+            url:'images/a.png'
+        }],
+        flash: {
+            url: "images/1.jpg_.webp",
+            sprice: "USD  18.2",
+            oldSprice: "USD 44"
+        }
+
+    },
+    mounted: function() {
+
+        this.$nextTick(function() {
+            // 代码保证 this.$el 在 document 中
+        })
+            setTimeout(function(){
+            $.ajax({
+                type: 'get',
+                url: '/data/banner',
+                dataType: 'json',
+                success: function(info) {
+                    console.log(JSON.stringify(info,null,2));
+                    app.banners = info.data;
+                    console.log(app.banners)
+                },
+                error:function(info){
+                       console.log(info);
+                }
+            });
+
+            },2000)
+       
     }
-    
-}
-}) 
+})
